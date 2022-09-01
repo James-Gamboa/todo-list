6a1528e1f70e491f1bfc3887089c019f390f69c7
James-Gamboa <jjguevarag@gmail.com>
2022-09-01 15:49:29 -0600

    agregue que se actualize la aplicacion y conserve datos

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const todos = [];
+// @ts-ignore
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
+const render =() =>{
+  const todoList = document.getElementById("todo-list");
+    // @ts-ignore
+    const todosTemplate = todos.map (t =>  "<li>" + t + "</li>");
+    // @ts-ignore
+    todoList.innerHTML = todosTemplate.join("");
+    const elementos = document.querySelectorAll("#todo-list li")
+    elementos.forEach((elemento,i)=>{
+      elemento.addEventListener("click", ()=>{
+        elemento.parentNode?.removeChild(elemento)
+        todos.splice(i, 1)
+        actualizaTodos(todos)
+        render()
+      })
+    })
+}
+
+const actualizaTodos = (todos) => {
+  const todoStrings = JSON.stringify(todos)
+  localStorage.setItem("todos",todoStrings)
+}
 window.onload = () => {
+  render()
   const form = document.getElementById("todo-form");
   // @ts-ignore
   form.onsubmit = (e) => {
@@ -11,12 +34,8 @@ window.onload = () => {
     // @ts-ignore
     todo.value = "";
     todos.push(todoText);
-    const todoList = document.getElementById("todo-list");
+    actualizaTodos(todos)
     // @ts-ignore
-    todoList.innerHTML = "";
-    for (let i =0 ; i < todos.length; i++) {
-      // @ts-ignore
-      todoList.innerHTML += "<li>" + todos [i] + "</li>";
-    }
+    render()
   }
 }
